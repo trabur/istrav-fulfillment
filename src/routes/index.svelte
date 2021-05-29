6b1c2ad2f0a3f315ec2b54077e22ae71b6663372
istrav <travis.burandt@gmail.com>
2021-05-28 19:50:10 -0500

    config backend

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,6 +1,7 @@
 <script>
   import { onMount } from "svelte";
   
+	import { appData } from '../stores.js';
   import { istrav, scripts } from '../../farmerless/api'
 
   import Generic1 from '../../farmerless/components/wireframes/Generic1.svelte'
@@ -10,47 +11,19 @@
 	import Slogan from '../../farmerless/components/blocks/Slogan.svelte'
 
   let app
-  let domainId
-  let state = 'production'
-
-	onMount(async () => {
-    domainId = window.location.host
-
-    // pick an app to show for local development
-    if (domainId.includes('localhost:5000')) {
-      domainId = 'istrav.com'
-    }
-    // set appId from domain 
-    if (domainId.includes('aaghc.com')) {
-      // for subdomains such as http://istrav.aaghc.com
-      let endpoint = domainId.split('.')[0]
-      let esEndpoint = await scripts.tenant.apps.getEndpoint(null, endpoint)
-      if (esEndpoint.payload.success === true) {
-        app = esEndpoint.payload.data
-        domainId = esEndpoint.payload.data.domain // do this so images load
-      } else {
-        alert(esEndpoint.payload.reason)
-      }
-    } else {
-      // for custom domains such as https://aaghc.com
-      domainId = domainId.split('.').slice(-2).join('.')
-      let esOne = await scripts.tenant.apps.getOne(null, domainId, state)
-      if (esOne.payload.success === true) {
-        app = esOne.payload.data
-      } else {
-        alert(esOne.payload.reason)
-      }
-    }
+  appData.subscribe(value => {
+    app = value
+    console.log(app)
   })
 </script>
 
 <svelte:head>
-  {#if app}
+  {#if app.id}
 	  <title>Message Bulletin Board - {app.labelName}</title>
   {/if}
 </svelte:head>
 
-{#if app}
+{#if app.id}
   <Generic1 showWiring={false}>
     <section slot="logo" class="slot">
       <Logo {app} fontSize='' height='' />
